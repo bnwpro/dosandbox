962e425c4a51d354277fd72eeaaae314313fd7c5
bnwpro <brendon@nwpro.org>
2024-02-06 01:12:47 -0500

    tweak database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,7 @@ set :puma_enable_socket_service, true
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", 'config/master.key'
+#append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
