718072c9ff474c173fc0b816e9f2336ec487fb13
bnwpro <brendon@nwpro.org>
2024-02-08 11:04:44 -0500

    temp werirdness

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :pty, true
 set :use_sudo, false
 set :stage, :production
 set :deploy_via, :remote_cache
-set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to, "/home/#{fetch(:user)}apps/#{fetch(:application)}"
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"#shared_path
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"#shared_path
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"#shared_path
