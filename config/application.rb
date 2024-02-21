06a831e434268b574820f54beeb2455c9c4efab2
bnwpro <brendon@nwpro.org>
2024-02-21 15:32:25 -0500

    Upgrade Turbo rails Hotwire

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Dosandbox
     # Use a real queuing backend for Active Job (and separate queues per environment).
     config.active_job.queue_adapter = :sidekiq
     # config.active_job.queue_name_prefix = "dosandbox_production"
+    Sidekiq.strict_args!(false)
   end
 end
