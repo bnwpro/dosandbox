32620acd04473ba956fc783ea255f09461e4a22c
bnwpro <brendon@nwpro.org>
2024-02-27 16:05:57 -0500

    Cable config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://elixirband.live/cable"
-  # config.action_cable.allowed_request_origins = [ "http://elixirband.live", /http:\/\/elixirband.*/ ]
+  config.action_cable.url = "wss://elixirband.live/cable"
+  config.action_cable.allowed_request_origins = [ "https://elixirband.live", /https:\/\/elixirband.*/ ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
